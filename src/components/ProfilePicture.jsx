c98783a061f59525f72758749b1002c104d4b6ad
RitikKumar202 <ritik2002july@gmail.com>
2023-08-20 23:23:46 +0530

    implemented upload profile picture functionality

--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -1,36 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import uploadFolderUrl from "../constants/uploadFolderUrl";
 import { HiOutlineCamera } from "react-icons/hi";
+import CropEasy from "./crop/CropEasy";
+import { createPortal } from "react-dom";
 
 const ProfilePicture = ({ avatar }) => {
+  const [openCrop, setOpenCrop] = useState(false);
+  const [photo, setPhoto] = useState(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setPhoto({ url: URL.createObjectURL(file), file });
+    setOpenCrop(true);
+  };
+
   return (
-    <div className="w-full bg-[rgba(0,0,0,.05)] py-5 px-8 rounded-md flex md:flex-col items-center justify-center gap-x-4 mb-2 md:mb-0 md:mr-9">
-      <div className="relative w-20 h-20 rounded-full outline outline-offset-2 outline-1 lutline-primary overflow-hidden">
-        <label
-          htmlFor="profilePicture"
-          className="cursor-pointer absolute inset-0 rounded-full bg-transparent"
+    <>
+      {openCrop &&
+        createPortal(
+          <CropEasy photo={photo} setOpenCrop={setOpenCrop} />,
+          document.getElementById("portal")
+        )}
+      <div className="w-full bg-[rgba(0,0,0,.05)] py-5 px-8 rounded-md flex md:flex-col items-center justify-center gap-x-4 mb-2 md:mb-0 md:mr-9">
+        <div className="relative w-20 h-20 rounded-full outline outline-offset-2 outline-1 lutline-primary overflow-hidden">
+          <label
+            htmlFor="profilePicture"
+            className="cursor-pointer absolute inset-0 rounded-full bg-transparent"
+          >
+            {avatar ? (
+              <img
+                src={uploadFolderUrl.UPLOAD_FOLDER_BASE_URL + avatar}
+                alt="profile"
+                className="w-full h-full object-fill"
+              />
+            ) : (
+              <div className="w-full h-full bg-blue-50/50 flex justify-center items-center">
+                <HiOutlineCamera className="w-7 h-auto text-primary" />
+              </div>
+            )}
+          </label>
+          <input
+            type="file"
+            className="sr-only"
+            id="profilePicture"
+            onChange={handleFileChange}
+          />
+        </div>
+        <button
+          type="button"
+          className="md:mt-4 border-none bg-red-500 rounded-md px-4 py-1 text-white"
         >
-          {avatar ? (
-            <img
-              src={uploadFolderUrl.UPLOAD_FOLDER_BASE_URL + avatar}
-              alt="profile"
-              className="w-full h-full object-fill"
-            />
-          ) : (
-            <div className="w-full h-full bg-blue-50/50 flex justify-center items-center">
-              <HiOutlineCamera className="w-7 h-auto text-primary" />
-            </div>
-          )}
-        </label>
-        <input type="file" className="sr-only" id="profilePicture" />
+          Remove
+        </button>
       </div>
-      <button
-        type="button"
-        className="md:mt-3 border-none bg-red-500 rounded-lg px-4 py-1 text-white"
-      >
-        Delete
-      </button>
-    </div>
+    </>
   );
 };
 
